4991d5ae223ae107086ed2843777c999befc800f
Mohit Karekar <mohit.karekar@zalando.de>
2025-02-06 18:09:19 +0100

    Add github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,10 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://gom-lang.github.io',
   vite: {
     plugins: [tailwindcss()]
   },
-
   integrations: [react({
     include: ['src/react/**/*.{astro,js,jsx,ts,tsx}'],
   })]
